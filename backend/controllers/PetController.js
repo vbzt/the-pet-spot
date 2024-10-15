8adb4bf6a07c4b093af4535b3e832ac2e182a6f4
vbzt <vitorcastrobuzato@gmail.com>
2024-10-14 21:03:45 -0300

    getting pets by id and user token now

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -2,6 +2,7 @@ const getToken = require('../helpers/get-token')
 const getUserByToken = require('../helpers/get-user-by-token')
 const petValidation = require('../helpers/pet-validation')
 const Pet = require('../models/Pets')
+const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = class PetController { 
 
@@ -34,5 +35,38 @@ module.exports = class PetController {
   static async getAll(req, res){
     const pets = await Pet.find().sort('-createdAT')
     res.status(200).json({pets})
+  }
+
+  static async getAllUserPets(req, res){ 
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    const pets = await Pet.find({'user._id': user._id}).sort('-createdAT')
+    res.status(200).json({pets})
+  }
+
+  static async getAllUserAdoptions(req,res){ 
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    const pets = await Pet.find({'adopter._id': user._id}).sort('-createdAT')
+    res.status(200).json({pets})
+  }
+
+  static async getPetById(req,res){ 
+    const id = req.params.id    
+    if(!ObjectId.isValid(id)) {
+      res.status(422).json({message: 'ID invalido'})
+      return
+    }
+
+    const pet = await Pet.findById(id)
+    if(!pet){
+      res.status(404).json({message: 'Pet não encontrado'})
+    }
+    res.status(200).json({pet})
+
+
+
   }
 }
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -8,6 +8,9 @@ const { imageUpload } = require('../helpers/image-uploader')
 
 
 router.get('/', PetController.getAll)
+router.get('/mypets', verifyToken,  PetController.getAllUserPets)
+router.get('/:id', PetController.getPetById)
+router.get('/myadoptions', verifyToken,  PetController.getAllUserAdoptions)
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create)
 
 module.exports = router 
