1ba72e8d179cdb7285819cb4eb8ad267ee1ec89f
vbzt <vitorcastrobuzato@gmail.com>
2024-10-20 17:42:48 -0300

    fixed api problems

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3002'}))
+const allowCrossDomain = (req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+app.use(allowCrossDomain);
 
 app.use(express.static('public'))
 app.use(express.json())
