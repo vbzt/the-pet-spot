28f1a42b08a0bd58c7df0ab53cfe7fb48c2cce75
vbzt <vitorcastrobuzato@gmail.com>
2024-10-22 12:37:16 -0300

    added accept (images) option

--- frontend/src/components/form/Input.jsx
+++ frontend/src/components/form/Input.jsx
@@ -1,9 +1,19 @@
 import styles from './input.module.css'
+
 const Input = ({type, text, name, placeholder, handleOnChange, value, multiple}) => {
   return (
-    <div className= {styles.formControl}>
+    <div className={styles.formControl}>
       <label htmlFor={name}>{text}: </label>
-      <input type={type} name={name} id={name} placeholder= {placeholder} onChange={handleOnChange} value={value} {...(multiple ? multiple : '')} />
+      <input 
+        type={type} 
+        name={name} 
+        id={name} 
+        placeholder={placeholder} 
+        onChange={handleOnChange} 
+        value={value}
+        accept={type === 'file' ? 'image/png, image/jpeg' : undefined}
+        multiple={multiple ? true : undefined}
+      />
     </div>
   )
 }
