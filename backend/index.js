645f9c1fe0e08b233666a521644055b1cba4fca7
vbzt <vitorcastrobuzato@gmail.com>
2024-10-22 11:55:00 -0300

    fixed cors, again

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,20 @@ const cors = require('cors')
 
 const app = express()
 
-const allowCrossDomain = (req, res, next) =>{
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-app.use(allowCrossDomain);
+app.use(cors({
+  origin: '*',
+  allowedHeaders: ['Authorization', 'Content-Type'], 
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+}))
 
 app.use(express.static('public'))
 app.use(express.json())
 
-//routes
 const UserRoutes = require("./routes/UserRoutes")
 const PetRoutes = require("./routes/PetRoutes")
 app.use('/users', UserRoutes)
 app.use('/pets', PetRoutes)
 
-app.listen(5000, ()=> { 
+app.listen(5000, () => {
   console.log('>> server on')
 })
